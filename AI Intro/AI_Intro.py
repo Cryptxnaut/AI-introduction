beabf36af42736f6e0883d960695d3aa373f7115
Cryptxnaut <jaiakilesh06@gmail.com>
2023-05-02 18:41:36 +0100

    Queue frontier class

--- AI Intro/AI_Intro.py
+++ AI Intro/AI_Intro.py
@@ -8,10 +8,41 @@ class Node():
         self.action = action 
 
 class StackFrontier():
-    def __init__(seft):
+    #last in first out data
+
+    #creates frontier using list
+    def __init__(seft): 
         self.frontier = []
     
+    #adds to the frontier appending it to the end of the list
     def add(self, node):
         self.frontier.append(node)
     
-    def contains_state()
+    #checks if frontier contains a state
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    #checks if frontier is empty e.g length = 0
+    def empty(self):
+        return len(self.frontier) == 0
+
+    #remove from the frontier
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[-1] #last item in list
+            return node 
+
+
+class QueueFrontier(StackFrontier):
+
+    #remove from the begining of a list
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
