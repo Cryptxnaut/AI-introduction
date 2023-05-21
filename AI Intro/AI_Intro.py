19e998fa528786bb345d71384c42f5e72d711829
Cryptxnaut <jaiakilesh06@gmail.com>
2023-05-21 21:17:04 +0100

    solve function

--- AI Intro/AI_Intro.py
+++ AI Intro/AI_Intro.py
@@ -35,7 +35,7 @@ class StackFrontier():
             self.frontier = self.frontier[-1] #last item in list
             return node 
 
-
+# Optional
 class QueueFrontier(StackFrontier):
 
     #remove from the begining of a list
@@ -46,3 +46,27 @@ class QueueFrontier(StackFrontier):
             node = self.frontier[0]
             self.frontier = self.frontier[1:]
             return node
+
+
+class Maze():
+    def __init__(self, filename):
+
+        with open(filename) as f:
+            contents = f.read()
+
+        if contents.count("A") != 1:
+            raise Exception("maze must have one start point")
+        if contents.count("B") != 1:
+            raise Exception("maze must have one goal")
+
+
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line)for line in contents)
+        
+        self.walls = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                try#...
+
